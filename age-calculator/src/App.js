25454653d84aad8eba950ea38fabf6dc9b418e95
Maria Cosmen <mcosme00@gmail.com>
2023-09-06 12:45:22 +0900

    Set App main container

--- age-calculator/src/App.js
+++ age-calculator/src/App.js
@@ -1,8 +1,14 @@
 import './index.css';
+import Form from './components/Form';
+import Result from './components/Result';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full h-screen flex bg-accent items-center justify-center">
+      <div className="p-10 bg-white w-[700px] h-[500px] rounded-br-max rounded-xl">
+        <Form />
+        <Result />
+      </div>
     </div>
   );
 }
